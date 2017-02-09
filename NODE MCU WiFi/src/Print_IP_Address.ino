3496a7f1929419922235171da736732b03f06fec
fuadcu@maestralsolutions.com <fuadcu@maestralsolutions.com>
2017-02-09 13:51:17 +0100

    WiFi
    WiFi Connection is working

--- NODE MCU WiFi/src/Print_IP_Address.ino
+++ NODE MCU WiFi/src/Print_IP_Address.ino
@@ -1,16 +1,21 @@
 #include "ESP8266WiFi.h"
 
-# define LED D4 // Use built-in LED which connected to D4 pin or GPIO 2
-
 // WiFi parameters to be configured
-const char* ssid = "MGUEST";
-const char* password = "guest+internet";
+const char* ssid = "ZTE-FC";
+const char* password = "foofoo73";
+
+WiFiClient client;
+
+// ThingSpeak Settings
+// const channelID = XXX;
+String writeAPIKey = "EO8CJUONQCLYCGA7"; // write API key for your ThingSpeak Channel
+const char* server = "api.thingspeak.com";
+const int postingInterval = 20 * 1000; // post data every 20 seconds
+
 
 void setup(void)
 {
-  pinMode(LED, OUTPUT);     // Initialize the LED as an output
-
-  Serial.begin(9600);
+  Serial.begin(115200);
   // Connect to WiFi
   WiFi.begin(ssid, password);
 
@@ -19,14 +24,14 @@ void setup(void)
   while (WiFi.status() != WL_CONNECTED) {
      delay(500);
      Serial.print(".");
-     digitalWrite(LED, LOW);   // Turn the LED on because the LED is active low
+
+
   }
   //print a new line, then print WiFi connected and the IP address
   Serial.println("");
   Serial.println("WiFi connected");
   // Print the IP address
   Serial.println(WiFi.localIP());
-  digitalWrite(LED, HIGH);  // Turn the LED off because the LED is active low
 
 }
 void loop() {
@@ -36,4 +41,33 @@ void loop() {
   digitalWrite(LED, LOW);   // Turn the LED on because the LED is active low
   delay(500);
   */
+
+  if (client.connect(server, 80)) {
+
+    // Measure Signal Strength (RSSI) of Wi-Fi connection
+    long rssi = WiFi.RSSI();
+
+    // Construct API request body
+    String body = "field1=";
+           body += String(rssi);
+
+    Serial.print("RSSI: ");
+    Serial.println(rssi);
+
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: " + writeAPIKey + "\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(body.length());
+    client.print("\n\n");
+    client.print(body);
+    client.print("\n\n");
+
+  }
+  client.stop();
+
+  // wait and then post again
+  delay(postingInterval);
 }
